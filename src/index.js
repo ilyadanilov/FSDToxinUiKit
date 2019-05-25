4734b1b5d989ec93332755bd782c3d19207f0396
Ilya Danilov <danilov.freelance@gmail.com>
2019-05-25 14:27:26 +0300

    Правки календаря, добавил инпут с фильтром даты

--- src/index.js
+++ src/index.js
@@ -6,8 +6,76 @@ var picker = new Lightpick({
   field: document.getElementById('datepick-1'),
   secondField: document.getElementById('datepick-2'),
   singleDate: false,
+  autoclose: false,
+  lang: 'ru',
+  locale: {
+    tooltip: {
+      one: 'день',
+      few: 'дня',
+      many: 'дней'
+    },
+    pluralize: function(i, locale) {
+      if ('one' in locale && i % 10 === 1 && !(i % 100 === 11)) return locale.one;
+      if (
+        'few' in locale &&
+        i % 10 === Math.floor(i % 10) &&
+        i % 10 >= 2 &&
+        i % 10 <= 4 &&
+        !(i % 100 >= 12 && i % 100 <= 14)
+      )
+        return locale.few;
+      if (
+        'many' in locale &&
+        (i % 10 === 0 ||
+          (i % 10 === Math.floor(i % 10) && i % 10 >= 5 && i % 10 <= 9) ||
+          (i % 100 === Math.floor(i % 100) && i % 100 >= 11 && i % 100 <= 14))
+      )
+        return locale.many;
+      if ('other' in locale) return locale.other;
+
+      return '';
+    }
+  },
   format: 'DD.MM.YYYY',
   footer: true,
-  repick: true,
-  inline: true
+  selectForward: true,
+  parentEl: '.input-date'
+});
+var filter = new Lightpick({
+  field: document.getElementById('input-filter'),
+  singleDate: false,
+  autoclose: false,
+  lang: 'ru',
+  locale: {
+    tooltip: {
+      one: 'день',
+      few: 'дня',
+      many: 'дней'
+    },
+    pluralize: function(i, locale) {
+      if ('one' in locale && i % 10 === 1 && !(i % 100 === 11)) return locale.one;
+      if (
+        'few' in locale &&
+        i % 10 === Math.floor(i % 10) &&
+        i % 10 >= 2 &&
+        i % 10 <= 4 &&
+        !(i % 100 >= 12 && i % 100 <= 14)
+      )
+        return locale.few;
+      if (
+        'many' in locale &&
+        (i % 10 === 0 ||
+          (i % 10 === Math.floor(i % 10) && i % 10 >= 5 && i % 10 <= 9) ||
+          (i % 100 === Math.floor(i % 100) && i % 100 >= 11 && i % 100 <= 14))
+      )
+        return locale.many;
+      if ('other' in locale) return locale.other;
+
+      return '';
+    }
+  },
+  format: 'DD.MM',
+  footer: true,
+  selectForward: true,
+  parentEl: '.input-filter'
 });
