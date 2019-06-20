09de115ae4ba5ac06348fd7b0a0430fbd644977e
ilyadanilov <danilov.freelance@gmail.com>
2019-06-20 16:41:40 +0300

    Add добавил значения  в рублях для слайдера

--- src/includes/formElements/slider/slider.js
+++ src/includes/formElements/slider/slider.js
@@ -3,10 +3,29 @@ var slider = document.querySelector('.slider');
 
 noUiSlider.create(slider, {
   start: [20, 80],
-  padding: [5, 5],
   connect: true,
   range: {
     min: 0,
     max: 100
-  }
+  },
+  format: {
+    // 'to' the formatted value. Receives a number.
+    to: function (value) {
+        return Number.parseInt(value);
+    },
+    // 'from' the formatted value.
+    // Receives a string, should return a number.
+    from: function (value) {
+        return Number.parseInt(value);
+    }
+}
+
+});
+const handleValues = [
+  document.querySelector('.value-lower'),
+  document.querySelector('.value-upper')
+];
+
+slider.noUiSlider.on('update', function (values, handle) {
+  handleValues[handle].innerHTML = `${values[handle]} ₽`;
 });
