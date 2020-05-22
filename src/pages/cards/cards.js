c80b75a90ab3dfade3506267480a3d40e6478104
Ilya Danilov <danilov.freelance@gmail.com>
2020-05-22 13:56:01 +0300

    Imported Lightpick for calendar

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,5 +1,51 @@
+require('./cards.pug');
+
 import '../../includes/formElements/date-picker/date-picker';
 import '../../includes/formElements/dropdown/dropdown';
 import '../../includes/formElements/dropdown/dropdown';
-import 'lightpick/css/lightpick.css';
+import 'lightpick/scss/lightpick.scss';
 import '../../scss/pages/_cards.scss';
+
+import Lightpick from 'lightpick';
+
+var cardDatePicker = new Lightpick({
+  field: document.getElementById('card-date'),
+  inline: true,
+  lang: 'ru',
+  locale: {
+    tooltip: {
+      one: 'день',
+      few: 'дня',
+      many: 'дней'
+    },
+    buttons: {
+      apply: 'Применить',
+      reset: 'Очистить'
+    },
+    pluralize: function(i, locale) {
+      if ('one' in locale && i % 10 === 1 && !(i % 100 === 11))
+        return locale.one;
+      if (
+        'few' in locale &&
+        i % 10 === Math.floor(i % 10) &&
+        i % 10 >= 2 &&
+        i % 10 <= 4 &&
+        !(i % 100 >= 12 && i % 100 <= 14)
+      )
+        return locale.few;
+      if (
+        'many' in locale &&
+        (i % 10 === 0 ||
+          (i % 10 === Math.floor(i % 10) && i % 10 >= 5 && i % 10 <= 9) ||
+          (i % 100 === Math.floor(i % 100) && i % 100 >= 11 && i % 100 <= 14))
+      )
+        return locale.many;
+      if ('other' in locale) return locale.other;
+
+      return '';
+    }
+  },
+  format: 'YYYY-MM-DD',
+  footer: true,
+  selectForward: true
+});
