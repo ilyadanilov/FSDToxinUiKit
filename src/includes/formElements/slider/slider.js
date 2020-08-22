50db82c15a4ec14ca59c7068322c4bacb4587540
Ilya Danilov <danilov.freelance@gmail.com>
2020-08-23 01:06:36 +0300

    Переписал классы для лучшей читаемости, добавил немного стилей

--- src/includes/formElements/slider/slider.js
+++ src/includes/formElements/slider/slider.js
@@ -1,31 +1,30 @@
-import noUiSlider from 'nouislider/distribute/nouislider.js';
-var slider = document.querySelector('.slider');
+import noUiSlider from "nouislider/distribute/nouislider.js";
+var slider = document.querySelector(".slider__control");
 
 noUiSlider.create(slider, {
   start: [20, 80],
   connect: true,
   range: {
     min: 0,
-    max: 100
+    max: 100,
   },
   format: {
     // 'to' the formatted value. Receives a number.
     to: function (value) {
-        return Number.parseInt(value);
+      return Number.parseInt(value);
     },
     // 'from' the formatted value.
     // Receives a string, should return a number.
     from: function (value) {
-        return Number.parseInt(value);
-    }
-}
-
+      return Number.parseInt(value);
+    },
+  },
 });
 const handleValues = [
-  document.querySelector('.value-lower'),
-  document.querySelector('.value-upper')
+  document.querySelector(".slider__value-lower"),
+  document.querySelector(".slider__value-upper"),
 ];
 
-slider.noUiSlider.on('update', function (values, handle) {
+slider.noUiSlider.on("update", function (values, handle) {
   handleValues[handle].innerHTML = `${values[handle]} ₽`;
 });
