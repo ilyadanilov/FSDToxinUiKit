6999692adf4928b7a61b3d8c653d760dfdc332d6
Ilya Danilov <danilov.freelance@gmail.com>
2020-09-15 18:46:06 +0300

    Fixed wrong path for css and to h&f from cards

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,85 +11,86 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    main: ['./src/main.js'],
-    cards: ['./src/pages/cards/cards.js'],
-    "headers-and-footers": ["./src/pages/headers-and-footers/headers-and-footers.js"],
+    main: ["./src/main.js"],
+    cards: ["./src/pages/cards/cards.js"],
+    "headers-and-footers": [
+      "./src/pages/headers-and-footers/headers-and-footers.js",
+    ],
   },
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: 'js/[name]-bundle.[contenthash].js',
-    path: path.resolve(__dirname, '../dist/'),
-    publicPath: './'
+    filename: "js/[name]-bundle.[contenthash].js",
+    path: path.resolve(__dirname, "../dist/"),
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       cacheGroups: {
         vendor: {
-          chunks: 'initial',
-          minChunks: 2
-        }
-      }
-    }
+          chunks: "initial",
+          minChunks: 2,
+        },
+      },
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: ['babel-loader', 'webpack-import-glob-loader'],
-        exclude: /node_modules/
+        use: ["babel-loader", "webpack-import-glob-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              url: false
-            }
-          }
-        ]
+              url: false,
+            },
+          },
+        ],
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              url: false
-            }
+              url: false,
+            },
           },
-          'postcss-loader',
-          'sass-loader',
-          'webpack-import-glob-loader'
-        ]
+          "postcss-loader",
+          "sass-loader",
+          "webpack-import-glob-loader",
+        ],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
             options: {
-              attr: ['img:src']
-            }
-          }
-        ]
+              attr: ["img:src"],
+            },
+          },
+        ],
       },
       {
         test: /\.pug$/,
-        use: 'pug-loader'
+        use: "pug-loader",
       },
       {
         test: /\.(jpg|gif|png)$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: 'images/[name].[ext]'
-          }
-        }
+            name: "images/[name].[ext]",
+          },
+        },
       },
       {
         test: /\.ttf$/,
@@ -102,27 +103,27 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        loader: 'svg-sprite-loader',
+        loader: "svg-sprite-loader",
         options: {
           extract: true,
-          spriteFilename: 'images/sprite.svg',
-        }
-      }
-    ]
+          spriteFilename: "images/sprite.svg",
+        },
+      },
+    ],
   },
   plugins: [
     new SpriteLoaderPlugin(),
     new OptimizeCssAssetsPlugin(),
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: './src/pages/cards/cards.pug',
-      filename: 'pages/cards.html',
-      excludeChunks: ['main', 'headers-and-footers']
+      template: "./src/pages/cards/cards.pug",
+      filename: "pages/cards.html",
+      excludeChunks: ["main", "headers-and-footers"],
     }),
     new HTMLWebpackPlugin({
-      template: './src/index.pug',
-      filename: 'index.html',
-      excludeChunks: ['cards','headers-and-footers']
+      template: "./src/index.pug",
+      filename: "index.html",
+      excludeChunks: ["cards", "headers-and-footers"],
     }),
     new HTMLWebpackPlugin({
       template: "./src/pages/headers-and-footers/headers-and-footers.pug",
@@ -130,21 +131,21 @@ module.exports = {
       excludeChunks: ["main", "cards"],
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash].css'
+      filename: "css/[name].[contenthash].css",
     }),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
     }),
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify('production') }
+      "process.env": { NODE_ENV: JSON.stringify("production") },
     }),
     new MinifyPlugin(),
     new CompressionPlugin({
-      algorithm: 'gzip'
+      algorithm: "gzip",
     }),
     new BrotliPlugin(),
     new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ru/),
-  ]
+  ],
 };
